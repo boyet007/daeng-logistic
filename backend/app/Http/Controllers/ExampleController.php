693a2e17bdd5b8f469b411a0f4ee628a2e64dcd1
boyet <boyet007@gmail.com>
2021-03-28 22:29:16 +0700

    Authenticate & Manage Users

--- backend/app/Http/Controllers/ExampleController.php
+++ backend/app/Http/Controllers/ExampleController.php
@@ -4,15 +4,9 @@ namespace App\Http\Controllers;
 
 class ExampleController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
+
+    public function index() {
+        return "test controller";
     }
 
-    //
 }
--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -18,9 +18,11 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email',
-    ];
+    // protected $fillable = [
+    //     'name', 'email',
+    // ];
+
+    protected $guarded = [];
 
     /**
      * The attributes excluded from the model's JSON form.
